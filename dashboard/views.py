38a5e542e959e111203e1d348dad0c673cd0c031
aiziks <isaacadedayo1@gmail.com>
2022-06-28 18:19:27 +0100

    Data Visualizer V1

---
+++ dashboard/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render , redirect
+from .models import CountryData
+from .forms import CountryData, CountryDataForm
+
+# Create your views here.
+
+def index(request):
+    data = CountryData.objects.all()
+
+    if request.method == 'POST':
+        print(request.POST)
+        form = CountryDataForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = CountryDataForm()
+
+    context = {
+        'data':data,
+        'form':form
+    }
+    return render(request , 'dashboard/index.html' , context)
